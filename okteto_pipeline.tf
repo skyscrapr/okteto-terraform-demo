849d0561575cb8bedc5422b842bc40fc874ef4b3
Richard Weerasinghe <richard@skyscrapr.io>
2023-10-24 21:30:28 +1300

    add pipeline resource

--- okteto_pipeline.tf
+++ okteto_pipeline.tf
@@ -1,20 +1,20 @@
-# resource okteto_pipeline "okteto_aws_s3" {
-#     name = "okteto_aws_s3"
-#     repo_url = "https://github.com/skyscrapr/oktetodo-terraform-s3.git"
-#     branch = "main"
+resource okteto_pipeline "okteto_aws_s3" {
+    name = "okteto_aws_s3"
+    repo_url = "https://github.com/skyscrapr/oktetodo-terraform-s3.git"
+    branch = "main"
 
-#     depends_on = [
-#         okteto_secret.aws_access_key_id,
-#         okteto_secret.aws_secret_access_key,
-#         okteto_secret.aws_region,
-#         aws_iam_user.okteto_deploy,
-#         aws_iam_user_policy_attachment.cloudformation_fullaccess,
-#         aws_iam_user_policy_attachment.iam_full_access,
-#         aws_iam_user_policy_attachment.lambda_full_access,
-#         aws_iam_user_policy_attachment.api_gateway_admin,
-#         aws_iam_user_policy_attachment.s3_full_access,
-#         aws_iam_user_policy_attachment.ddb_full_access,
-#         aws_iam_user_policy_attachment.admin_access,
-#         aws_iam_access_key.okteto_deploy
-#     ]
-# }
+    depends_on = [
+        okteto_secret.aws_access_key_id,
+        okteto_secret.aws_secret_access_key,
+        okteto_secret.aws_region,
+        aws_iam_user.okteto_deploy,
+        aws_iam_user_policy_attachment.cloudformation_fullaccess,
+        aws_iam_user_policy_attachment.iam_full_access,
+        aws_iam_user_policy_attachment.lambda_full_access,
+        aws_iam_user_policy_attachment.api_gateway_admin,
+        aws_iam_user_policy_attachment.s3_full_access,
+        aws_iam_user_policy_attachment.ddb_full_access,
+        aws_iam_user_policy_attachment.admin_access,
+        aws_iam_access_key.okteto_deploy
+    ]
+}
